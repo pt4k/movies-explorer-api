1e64db04260b4c17307b810e2b35f4308f729b51
Yuriy Pishchenko <locbne@gmail.com>
2022-10-04 19:36:17 +0300

    fix: отредактировал валидацию

--- middlewares/validatons.js
+++ middlewares/validatons.js
@@ -12,36 +12,36 @@ const validateAuthentication = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
 const valedateUserData = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().min(2).max(30),
-    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
   }),
 });
 
 const validateMovieData = celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(30),
-    director: Joi.string().required().min(2).max(30),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
     duration: Joi.number().required(),
-    year: Joi.string().required().min(2).max(4),
+    year: Joi.string().required(),
     description: Joi.string().required(),
     image: Joi.string().required().pattern(urlRegExp),
     trailerLink: Joi.string().required().pattern(urlRegExp),
     thumbnail: Joi.string().required().pattern(urlRegExp),
     movieId: Joi.number().required(),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
   }),
 });
 
 const validateMovieId = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
+    _id: Joi.string().required().alphanum().length(24),
   }),
 });
 
